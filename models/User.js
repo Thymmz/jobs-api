de454ce55aba9e6fc4242ff41c7469642a432066
Thymmz <atharva.deshpande228@gmail.com>
2022-02-18 13:32:58 -0500

    jwt setup done

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const UserSchema = new mongoose.Schema({
    name:{
@@ -13,12 +15,24 @@ const UserSchema = new mongoose.Schema({
       match:[/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/],
       unique: true,
    },
-   name:{
+   password:{
       type: String,
       require:[true, 'Please provide password'],
       minlength:6,
-      maxlength:12,
    },
 })
+
+UserSchema.pre('save', async function(next){
+
+   const salt = await bcrypt.genSalt(10)
+   this.password = await bcrypt.hash(this.password, salt)
+   next()
+})
+
+UserSchema.methods.createJWT = function() {
+   return jwt.sign({userId:this._id, name:this.name},process.env.JWT_SECRET, {
+      expiresIn:process.env.JWT_LIFETIME,
+   })
+}
 
 module.exports = mongoose.model('User', UserSchema)
